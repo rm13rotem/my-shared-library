64a43e2769895a7551ed56aa11aa9f21559768c9
Rotem Meron <rm13rotem@gmail.com>
2026-05-17 20:26:20 +0300

    Update sonarCreateProject and sonarLocalScan functions

--- vars/codeQuality.groovy
+++ vars/codeQuality.groovy
@@ -1,22 +1,35 @@
 def sonarCreateProject(String projectKey) {
+
+    withCredentials([
+        string(credentialsId: 'sonnar-scanner', variable: 'SONAR_TOKEN')
+    ]) {
+
         withSonarQubeEnv('SonarQubeScanner') {
+
             sh """
-                curl -s -u ${env.SONAR_TOKEN}: \
-               -X POST "${env.SONAR_HOST_URL}/api/projects/create" \
-               -d "project=${projectKey}&name=${projectKey}"
+                curl -s -u ${SONAR_TOKEN}: \
+                -X POST "${SONAR_HOST_URL}/api/projects/create" \
+                -d "project=${projectKey}&name=${projectKey}" || true
             """
         }
+    }
 }
 
 def sonarLocalScan() {
+
     def scannerHome = tool 'SonarQubeScanner'
+
     withSonarQubeEnv('SonarQubeScanner') {
+
         sh """
             ${scannerHome}/bin/sonar-scanner \
             -Dsonar.projectKey=${env.JOB_NAME} \
             -Dsonar.projectName=${env.JOB_NAME} \
             -Dsonar.sources=. \
-            -Dsonar.sourceEncoding=UTF-8
+            -Dsonar.sourceEncoding=UTF-8 \
+            -Dsonar.exclusions=**/node_modules/**,**/bin/**,**/obj/**
         """
     }
 }
+
+return this
